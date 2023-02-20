acd2b12d1fa4b9c3b71b47fe81eecefa5dafa18d
Crewen <nithinkrishna330@gmail.com>
2023-02-20 23:30:50 +0530

    Initial Commit

--- port_scanner.py
+++ port_scanner.py
@@ -5,30 +5,30 @@ import sys
 import pyfiglet
 import threading
 
-# simple Banner
+
 ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
 print(ascii_banner)
 
-usage = "python3 port_scanner.py ADDRESS START_PORT END_PORT"  #uscase of this scanner
+usage = "python3 port_scanner.py IP_ADDRESS START_PORT END_PORT" 
 
 if(len(sys.argv)!=4):
     print(usage)
     sys.exit()
 
 
-address =socket.gethostbyname(sys.argv[1])  #ip address
+address =socket.gethostbyname(sys.argv[1])  
 
 start_port = int(sys.argv[2])
 end_port = int(sys.argv[3])
 
-#simple patterns
+
 print("-"*100)
 print("Scanning ip: " + address)
 print("-"*100)
 
 
 try:
-    #scanning the open ports of target ip
+    
     def scan_port(port):
         print("Scanning port: ",port)
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -41,11 +41,17 @@ try:
 
     for port in range (start_port,end_port+1):                           
 
-        thread= threading.Thread( address = scan_port, args= (port,))   #threading
+        thread= threading.Thread( target= scan_port, args= (port,))  
         thread.start()
 
 except KeyboardInterrupt:
     print("\n Exiting...!!")
-    sys.exit()           # closing the application;
+    sys.exit() 
+
+except socket.error:
+
+     print("/n Not Responding..! ")
+     sys.exit()
+               
 
 
